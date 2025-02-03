9766cf821f45020ef9dc97bb7d2aee957e22ee8e
yunyoung1819 <yunyoung1819@hanmail.net>
2025-02-03 19:19:42 +0900

    feat: Initial commit

---
+++ coupon-api/src/main/java/com/zero/couponapi/CouponApiApplication.java
@@ -0,0 +1,13 @@
+package com.zero.couponapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApiApplication.class, args);
+    }
+
+}
